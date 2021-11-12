f83d7317d9fba216ec1c648788dcbf3be483c326
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-11-12 17:36:53 -0300

    Tela de cadastro completa

--- sistemaDeCadastro/src/App.js
+++ sistemaDeCadastro/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Formulario from './components/fomulario/Formulario';
 
 function App() {
   return (
     <>
-      <div><h1>Formulário</h1></div>
-      <div><h1>Editar Formulário</h1></div>
-      <div><h1>Listar Usuários</h1></div>
+      <div>
+        {/* <h1>Formulário</h1> */}
+        <Formulario/>
+      </div>
+      {/* <div><h1>Editar Formulário</h1></div>
+      <div><h1>Listar Usuários</h1></div> */}
     </>
   );
 }
---
+++ sistemaDeCadastro/src/components/fomulario/Formulario.js
@@ -0,0 +1,58 @@
+import {Formik} from 'formik';
+import styles from './Formulario.module.css'
+
+function Formulario() {
+    return (
+        <div>
+
+        
+        <form action="">
+            <div className={styles.title}>Cadastrar</div>
+          <div className={styles.form}>
+
+          <div className={styles.input}>
+                <h4>Nome</h4>
+                <input type="text" name="firstName" id="firstName" placeholder="Digite seu nome"/>   
+           </div>
+
+            
+            <div className={styles.input}>
+                <h4>Sobrenome</h4>
+                <input type="text" name="lastName" id="lastName" placeholder="Digite seu sobrenome"/>
+            </div>
+           
+           <div className={styles.input}>
+                <h4>Email</h4>
+                <input type="text" name="email" id="email" placeholder="Digite seu e-mail"/>
+           </div>
+            
+            <div className={styles.input}>
+                <h4>Endereço</h4>
+                <input type="text" name="address" id="address"  placeholder="Digite seu endereço"/>
+            </div>
+            
+            <div className={styles.input}>
+                <h4>Telefone</h4>
+               <input type="text" name="cellPhoneNumber" id="cellPhoneNumber" placeholder="Digite seu telefone"/>
+            </div>
+
+            <div className={styles.btn}>
+                <button>Cadastrar</button>
+            </div>
+          </div>
+
+          
+        </form>
+
+
+
+
+
+           
+        </div>
+    )
+}
+
+export default Formulario
+
+
