34eb3ebb5e6beb12d31f41948bb2226e3f46a662
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-11-14 18:24:30 -0300

    Trabalhando na função editar

--- sistemaDeCadastro/src/components/list/List.js
+++ sistemaDeCadastro/src/components/list/List.js
@@ -1,19 +1,42 @@
-
+import styless from '../fomulario/Formulario.module.css'
 import style from "../list/List.module.css"
 import styles from "../card/Card.module.css"
 import { AiFillDelete } from "@react-icons/all-files/ai/AiFillDelete";
 import { AiFillEdit } from "@react-icons/all-files/ai/AiFillEdit";
+import { useState } from 'react';
 
-const List = ({List,setUserList}) => {
+const List = ({formik,List,setUserList}) => {
 
   function removeUser(id){
       setUserList(List.filter(user => user.id !== id));
+  }
+
+  const [activeCard,setActiveCard] = useState(true);
+  const [activeEdit,setActiveEdit] = useState(false);
+
+  function active(){
+    if (activeCard === true) {
+      setActiveCard = false;
+      setActiveEdit = true;
+    }
+    else {
+      setActiveCard = true;
+      setActiveEdit = false;
+    }
+  }
+
+  function editUser(id){
+    const userFound = List.find(user => user.id === id)
+    formik.setValues(userFound)
   }
 
 
   return (
 
     <>
+
+
+
     
       <div className={style.list}>
       <div className={style.header}>
@@ -25,6 +48,8 @@ const List = ({List,setUserList}) => {
         List.length > 0 && (
           List.map(user => {
             return (
+
+              <div>
               <div className={styles.container}>
               <div className={styles.card}>
                 <div id={user.id}>
@@ -47,12 +72,59 @@ const List = ({List,setUserList}) => {
                 </div>
                 <div className={styles.btn}>
                   <button onClick={() => removeUser(user.id)} className={styles.delete} ><AiFillDelete className={styles.deleteIcon}/></button>
-                  <button className={styles.edit}><AiFillEdit /></button>
+                  <button onClick={() => editUser(user.id) }className={styles.edit}><AiFillEdit /></button>
                 </div>
               </div>
             </div>
+
+
+
+            <form onSubmit={formik.handleSubmit}>
+            <div className={styless.title}>Ediatr</div>
+          <div className={styless.form}>
+
+          <div className={styless.input}>
+                <h4 htmlFor="firstName">Nome</h4>
+                <input type="text" name="firstName" id="firstName" placeholder="Digite seu nome" onChange={formik.handleChange} value={formik.values.firstName}/>   
+           </div>
+
+            
+            <div className={styless.input}>
+                <h4 htmlFor="lastName">Sobrenome</h4>
+                <input type="text" name="lastName" id="lastName" placeholder="Digite seu sobrenome" onChange={formik.handleChange} value={formik.values.lastName}/>
+            </div>
+           
+           <div className={styless.input}>
+                <h4 htmlFor="email">Email</h4>
+                <input type="text" name="email" id="email" placeholder="Digite seu e-mail" onChange={formik.handleChange} value={formik.values.email}/>
+           </div>
+            
+            <div className={styless.input}>
+                <h4 htmlFor="address">Endereço</h4>
+                <input type="text" name="address" id="address"  placeholder="Digite seu endereço" onChange={formik.handleChange} value={formik.values.address} />
+            </div>
+            
+            <div className={styless.input}>
+                <h4 htmlFor="telefone">Telefone</h4>
+               <input type="text" name="cellPhoneNumber" id="cellPhoneNumber" placeholder="Digite seu telefone" onChange={formik.handleChange} value={formik.values.cellPhoneNumber}/>
+            </div>
+            
+            <div className={styless.btn}>
+                <button type="submit">Editar</button>
+            </div>
+            
+          </div>
+
+          
+        </form>
+
+              
+            </div>
+
             )
           })
+
+          
         )
       }
         <div className={style.btn}>
